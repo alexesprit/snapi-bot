879e9cf3cf632877102c7acea6a394cad048b25c
alexesprit <alex.esprit@gmail.com>
2010-10-23 14:20:06 +0000

    * revert votes (old variant)
    git-svn-id: http://snapi-bot.googlecode.com/svn/trunk@100 f251a5fa-d1d5-fdf8-25d6-16410800f524

--- plugins/votes.py
+++ plugins/votes.py
@@ -1,7 +1,8 @@
-# coding: utf-8
+# coding: utf-8;
 
-# votes.py
-# Initial Copyright (c) 2010 -Esprit-
+# vote.py
+# Initial Copyright (c) 2002-2005 Mike Mintz <mikemintz@gmail.com>
+# Modification Copyright (c) 2007 Als <Als@exploit.in>
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,259 +16,242 @@
 
 VOTES_FILE = "votes.txt"
 
-gVotes = {}
+VOTE_CREATED = 0x1
+VOTE_STARTED = 0x2
+VOTE_FINISHED = 0x3
 
-VOTE_STARTED = 0x1
-VOTE_FINISHED = 0x2
+gVote = {}
 
-VOTE_TEXT = 0x0
-VOTE_STATE = 0x1
-VOTE_VOTED = 0x2
-VOTE_CREATOR = 0x3
-VOTE_OPINIONS = 0x4
+def getVoteText(conference):
+	voteText = u"Голосование\nСоздатель: %(creator)s\n%(text)s\n" % (gVote[conference])
+	items = [u" * %s" % (x[0]) for x in gVote[conference]["opinions"]]
+	voteText += "\n".join(items)
+	voteText += u"\nЧтобы проголосовать, напиши номер мнения, например \"мнение 1\""
+	return voteText
 
-VOTE_SHOW_OPINIONS = 0x1
-VOTE_SHOW_FIRSTONLY = 0x2
-VOTE_SHOW_SCORES = 0x4
+def getVoteResults(conference):
+	answers = [[x[1], x[0]] for x in gVote[conference]["opinions"]]
+	answers.sort()
+	answers.reverse()
+	voteText = u"Результаты голосования\nСоздатель: %(creator)s\n%(text)s\n%%s" % (gVote[conference])
+	items = [u" * %s (%d место и %d голосов)" % (x[1], i + 1, x[0]) for i, x in enumerate(answers)]
+	return voteText % ("\n".join(items))
 
-voteTemplate = u"голосование создано! Чтобы показать его, напишите \"голосование %d\". \
-Чтобы проголосовать напишите \"мнение %d <номер_мнения>\". \
-Закончить голосование - \"итоги %d\""
-
-def getFreeID(base):
-	id = 1
-	while(True):
-		if(id not in base):
-			return(id)
-		id += 1
-
-def createOpinions(opinions):
-	opinionPoints = []
-	for opinion in opinions:
-		opinionPoints.append([0, opinion])
-	return(opinionPoints)
-
-def getOpinionScores(vote):
-	scores = 0
-	for opinion in vote[VOTE_OPINIONS]:
-		scores += opinion[0]
-	return(scores)
-
-def getVoteText(vote, flags=0x0):
-	voteText = vote[VOTE_TEXT]
-	if(flags & VOTE_SHOW_OPINIONS):
-		if(flags & VOTE_SHOW_SCORES):
-			voteOpinions = sorted(vote[VOTE_OPINIONS])
-			voteOpinions.reverse()
-		else:
-			voteOpinions = vote[VOTE_OPINIONS]
-		if(flags & VOTE_SHOW_FIRSTONLY):
-			voteOpinions = voteOpinions[:1]
-		for opinion in voteOpinions:
-			scores, text = opinion
-			if(flags & VOTE_SHOW_SCORES):
-				voteText += u"\n * %s (%s голосов)" % (text, scores)
-			else:
-				voteText += u"\n * %s" % (text)
-	return(voteText)
-
-def showVotes(msgType, conference, nick, param):
-	if(param and param.isdigit()):
-		voteID = int(param)
-		try:
-			vote = gVotes[conference][voteID]
-		except(KeyError):
-			sendMsg(msgType, conference, nick, u"такого голосования нет!")
-			return
-		if vote[VOTE_STATE] != VOTE_FINISHED:
-			hint = u"Чтобы проголосовать, напишите \"мнение %d <номер>\", например \"мнение %d 1\"" % (voteID, voteID)
-		else:
-			hint = u"Это голосование уже завершено"
-		sendMsg(msgType, conference, nick, u"%s\n\n%s" % (getVoteText(vote, VOTE_SHOW_OPINIONS), hint))
-	elif(not param):
-		if(not gVotes[conference].isEmpty()):
-			voteText = u"Текущие голосования:\n"
-			for i, voteID in enumerate(gVotes[conference]):
-				vote = gVotes[conference][voteID]
-				voteText += u"%d) %s" % (i + 1, getVoteText(vote))
-				if(vote[VOTE_STATE] != VOTE_FINISHED):
-					voteText += u"\nНе завершено, голосов: %d\n\n" \
-								% (getOpinionScores(vote))
-				else:
-					voteText += u"\nЗавершено, голосов: %d\n\n" \
-								% (getOpinionScores(vote))
-			voteText += u"подробнее - \"голосование <номер>\", например: \"голосование 1\""
-			sendMsg(msgType, conference, nick, voteText)
-		else:
-			sendMsg(msgType, conference, nick, u"голосований нет");	
-
-def createVote(msgType, conference, nick, param):
-	rawData = param.split("\n")
-	if(len(rawData) > 1):
-		text, opinions = rawData[0], rawData[1:]
-		vote = {}
-		vote[VOTE_TEXT] = text
-		vote[VOTE_STATE] = VOTE_STARTED
-		vote[VOTE_VOTED] = []
-		vote[VOTE_CREATOR] = [nick, getTrueJid(conference, nick)]
-		vote[VOTE_OPINIONS] = createOpinions(opinions)
-		voteID = getFreeID(gVotes[conference])
-		gVotes[conference][voteID] = vote
-		gVotes[conference].save()
-		sendMsg(msgType, conference, nick, voteTemplate % (voteID, voteID, voteID))
-	else:
-		sendMsg(msgType, conference, nick, u"читай справку по команде")
-
-def deleteVote(msgType, conference, nick, param):
-	if(param.isdigit()):
-		try:
-			voteID = int(param)
-			vote = gVotes[conference][voteID]
-			trueJid = getTrueJid(conference, nick)
-			creatorJid = vote[VOTE_CREATOR][1]
-			userAccess = getAccess(conference, trueJid)
-			if(trueJid == creatorJid or userAccess >= 20):
-				del(gVotes[conference][voteID])
-				gVotes[conference].save()
-				sendMsg(msgType, conference, nick, u"удалила")
-			else:
-				sendMsg(msgType, conference, nick, u"недостаточно прав")
-		except(IndexError):
-			sendMsg(msgType, conference, nick, u"такого голосования нет!")
-	elif(param == u"все"):
-		trueJid = getTrueJid(conference, nick)
-		userAccess = getAccess(conference, trueJid)
-		if(userAccess >= 30):		
-			gVotes[conference].clear()
-			gVotes[conference].save()
-			sendMsg(msgType, conference, nick, u"удалила")
-		else:
-			sendMsg(msgType, conference, nick, u"недостаточно прав")
-			
-def showOpinions(msgType, conference, nick, param):
-	if(not gVotes[conference].isEmpty()):
-		if(param and param.isdigit()):
-			voteID = int(param)
-			try:
-				vote = gVotes[conference][voteID]
-				votes = [vote]
-				flags = VOTE_SHOW_OPINIONS | VOTE_SHOW_SCORES
-			except(KeyError):
-				sendMsg(msgType, conference, nick, u"такого голосования нет!")
-				return
-		else:
-			votes = [gVotes[conference][i] for i in gVotes[conference]]
-			flags = VOTE_SHOW_OPINIONS | VOTE_SHOW_FIRSTONLY | VOTE_SHOW_SCORES
-		voteText = []
-		userJid = getTrueJid(conference, nick)
-		for i, vote in enumerate(votes):
-			creatorJid = vote[VOTE_CREATOR][1]
-			voteState = vote[VOTE_STATE]
-			if(creatorJid == userJid or voteState == VOTE_FINISHED):
-				voteText.append(u"%d) %s" % (i + 1, getVoteText(vote, flags)))
-			else:
-				voteText.append(u"%d) %s\nРезультаты недоступны" % (i + 1, getVoteText(vote)))
-		if(protocol.TYPE_PUBLIC == msgType):
-			sendMsg(msgType, conference, nick, u"ушли в приват")
-		sendMsg(protocol.TYPE_PRIVATE, conference, nick, u"Результаты голосований:\n%s" % ("\n\n".join(voteText)))
-	else:
-		sendMsg(msgType, conference, nick, u"голосований нет")
+def saveVotes(conference):
+	fileName = getConfigPath(conference, VOTES_FILE)
+	utils.writeFile(fileName, str(gVote[conference]))
 
 def vote(msgType, conference, nick, param):
-	param = param.split()
-	if(len(param) == 2):
-		try:
-			voteID = int(param[0])
-			opinionNum = int(param[1]) - 1
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"голосование было завершено")
+		elif(gVote[conference]["state"] == VOTE_CREATED):
+			sendMsg(msgType, conference, nick, u"голосование ещё не запущено")
+		else:
 			trueJid = getTrueJid(conference, nick)
-			vote = gVotes[conference][voteID]
-			if vote[VOTE_STATE] != VOTE_FINISHED:
-				if(trueJid not in vote[VOTE_VOTED]):
-					opinion = vote[VOTE_OPINIONS][opinionNum]
-					opinion[0] += 1
-					vote[VOTE_VOTED].append(trueJid)
-					gVotes[conference].save()
+			if(not trueJid in gVote[conference]["voted"]):
+				gVote[conference]["voted"][trueJid] = False
+			if(not gVote[conference]["voted"][trueJid]):
+				try:
+					n = int(param) - 1
+					gVote[conference]["opinions"][n][1] += 1
+					gVote[conference]["voted"][trueJid] = True
+					saveVotes(conference)
 					sendMsg(msgType, conference, nick, u"поняла")
-				else:
-					sendMsg(msgType, conference, nick, u"ты и так уже голосовал :-P")
+				except(IndexError, ValueError):
+					sendMsg(msgType, conference, nick, u"нет такого пункта")
 			else:
-				sendMsg(msgType, conference, nick, u"это голосование завершено")
-		except(KeyError, IndexError, ValueError):
-			sendMsg(msgType, conference, nick, u"читай справку по команде")
+				sendMsg(msgType, conference, nick, u"2-ой раз голосовать не надо :P")
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def createNewVote(msgType, conference, nick, param):
+	if param:
+		if(gVote[conference] and gVote[conference]["state"] != VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"имеется неоконченное голосование")
+		else:
+			trueJid = getTrueJid(conference, nick)
+			gVote[conference] = {"state": VOTE_CREATED, "creator": nick, "creatorjid": getTrueJid(conference, nick), "text": param, "voted": {}, "opinions": []}
+			saveVotes(conference)
+			sendMsg(msgType, conference, nick, u"Голосование создано! Чтобы добавить пункты напиши \"пункт+ твой_пункт\", удалить - \"пункт- номер пункта\". Начать голосование - команда \"голосование+\". Посмотреть текущие результаты - команда \"мнения\". Окончить голосование - команда \"итоги\"")
+	else:
+		if(gVote[conference]):
+			if(gVote[conference]["state"] == VOTE_FINISHED):
+				sendMsg(msgType, conference, nick, getVoteResults(conference))
+			elif(gVote[conference]["state"] == VOTE_STARTED):
+				sendMsg(msgType, conference, nick, getVoteText(conference))
+			else:
+				sendMsg(msgType, conference, nick, u"голование создано, но ещё не запущено")
+		else:
+			sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def startVote(msgType, conference, nick, parameters):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_STARTED):
+			sendMsg(msgType, conference, nick, u"голосование уже запущено")
+		elif(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"голосование было завершено")
+		elif(not gVote[conference]["opinions"]):
+			sendMsg(msgType, conference, nick, u"голосование не имеет пунктов")
+		else:
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				gVote[conference]["state"] = VOTE_STARTED
+				saveVotes(conference)
+				sendToConference(conference, getVoteText(conference))
+			else:
+				sendMsg(msgType, conference, nick, u"недостаточно прав")	  
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def stopVote(msgType, conference, nick, param):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"неприменимо к оконченному голосованию")
+		elif(gVote[conference]["state"] == VOTE_STARTED):
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				gVote[conference]["state"] = VOTE_CREATED
+				saveVotes(conference)
+				sendMsg(msgType, conference, nick, u"голосование приостановлено")
+			else:
+				sendMsg(msgType, conference, nick, u"ага, щаззз")
+		else:
+			sendMsg(msgType, conference, nick, u"голосование уже приостановлено")
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def addOpinion(msgType, conference, nick, param):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_STARTED):
+			sendMsg(msgType, conference, nick, u"неприменимо к запущеному голосованию, останови и добавь пункты")
+		elif(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"неприменимо к оконченному голосованию")
+		else:
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				if(param in [x[0] for x in gVote[conference]["opinions"]]):
+					sendMsg(msgType, conference, nick, u"уже есть такой пункт")
+				else:
+					gVote[conference]["opinions"].append([param, 0])
+					saveVotes(conference)
+					sendMsg(msgType, conference, nick, u"добавила")
+			else:
+				sendMsg(msgType, conference, nick, u"недостаточно прав")
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def delOpinion(msgType, conference, nick, param):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_STARTED):
+			sendMsg(msgType, conference, nick, u"неприменимо к запущеному голосованию, останови и добавь пункты")
+		elif(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"неприменимо к оконченному голосованию")
+		else:
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				try:
+					n = int(param) - 1
+					del(gVote[conference]["opinions"][n])
+					saveVotes(conference)
+					sendMsg(msgType, conference, nick, u"удалила")
+				except(KeyError, IndexError):
+					sendMsg(msgType, conference, nick, u"нет такого пункта")
+			else:
+				sendMsg(msgType, conference, nick, u"недостаточно прав")
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
+
+def showOpinions(msgType, conference, nick, param):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, getVoteResults(conference))
+		else:
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				if(protocol.TYPE_PUBLIC == msgType):
+					sendMsg(msgType, conference, nick, u"ушли в приват")
+				sendMsg(protocol.TYPE_PRIVATE, conference, nick, getVoteResults(conference))
+			else:
+				sendMsg(msgType, conference, nick, u"жди окончания голосования :-p")
+	else:
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
 
 def endVote(msgType, conference, nick, param):
-	if(not gVotes[conference].isEmpty()):
-		if(param and param.isdigit()):
-			voteID = int(param)
-			try:
-				vote = gVotes[conference][voteID]
-				if(VOTE_FINISHED != vote[VOTE_STATE]):
-					trueJid = getTrueJid(conference, nick)
-					creatorJid = vote[VOTE_CREATOR][1]
-					userAccess = getAccess(conference, trueJid)
-					if(trueJid == creatorJid or userAccess >= 20):
-						vote[VOTE_STATE] = VOTE_FINISHED
-						del(vote[VOTE_VOTED])
-						gVotes[conference].save()
-
-						flags = VOTE_SHOW_OPINIONS | VOTE_SHOW_SCORES
-						message = u"Результаты:\n%s" % (getVoteText(vote, flags))
-						sendMsg(msgType, conference, nick, message)
-					else:
-						sendMsg(msgType, conference, nick, u"недостаточно прав")
-				else:
-					showOpinions(msgType, conference, nick, param);		
-			except(KeyError):
-				sendMsg(msgType, conference, nick, u"такого голосования нет!")
-				return
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_FINISHED):
+			sendMsg(msgType, conference, nick, u"голосование уже закончено, просмотр мнений - команда \"мнения\"")
 		else:
-			showOpinions(msgType, conference, nick, None)
+			trueJid = getTrueJid(conference, nick)
+			creatorJid = gVote[conference]["creatorjid"]
+			if(creatorJid == trueJid or getAccess(conference, trueJid) >= 20):
+				gVote[conference]["state"] = VOTE_FINISHED
+				del(gVote[conference]["voted"])
+				saveVotes(conference)
+				sendToConference(conference, getVoteResults(conference))
+			else:
+				sendMsg(msgType, conference, nick, u"недостаточно прав")
 	else:
-		sendMsg(msgType, conference, nick, u"голосований нет")
+		sendMsg(msgType, conference, nick, u"сейчас нет никаких голосований")
 
-def sendNewVotes(conference, nick, trueJid, role, aff):
-	for voteID in gVotes[conference]:
-		vote = gVotes[conference][voteID]
-		if(vote[VOTE_STATE] != VOTE_FINISHED):
-			if(trueJid not in vote[VOTE_VOTED]):
-				voteText = u"Голосование\n%s" % (getVoteText(vote, VOTE_OPINIONS))
-				voteText += u"\n\nЧтобы проголосовать напишите \"мнение %d <номер_мнения>\"" % (voteID)
-				sendMsg(protocol.TYPE_PRIVATE, conference, nick, voteText)
+def showVote(conference, nick, trueJid, aff, role):
+	if(gVote[conference]):
+		if(gVote[conference]["state"] == VOTE_STARTED):
+			if(not trueJid in gVote[conference]["voted"]):
+				gVote[conference]["voted"][trueJid] = False
+				saveVotes(conference)
+				sendMsg(protocol.TYPE_PRIVATE, conference, nick, getVoteText(conference))
 
 def loadVotes(conference):
 	fileName = getConfigPath(conference, VOTES_FILE)
-	gVotes[conference] = database.DataBase(fileName)
+	utils.createFile(fileName, "{}")
+	gVote[conference] = eval(utils.readFile(fileName))
 
+registerJoinHandler(showVote)
 registerEvent(loadVotes, ADDCONF)
-registerJoinHandler(sendNewVotes)
 
-registerCommand(createVote, u"голосование+", 
-				11, u"Создаёт новое голосование", 
-				u"голосование+ <текст>\n<пункт>\n<пункт>\n...", 
-				(u"голосование винды - сакс!\nда\nнет!!"), 
+registerCommand(vote, u"мнение", 10, 
+				u"Для подачи мнения в текущем голосовании", 
+				u"мнение <номер>", 
+				(u"мнение 1", ), 
 				CHAT | PARAM)
-registerCommand(deleteVote, u"голосование-", 11, 
-				u"Удаляет голосование", 
-				u"голосование- <номер>", 
-				(u"голосование- 5", ), 
-				CHAT | PARAM)
-registerCommand(showVotes, u"голосования", 11, 
-				u"Показывает все голосования или какое-либо одно", 
-				u"голосование [номер]", 
-				(u"голосование", u"голосование 4"), 
+registerCommand(createNewVote, u"голосование", 11, 
+				u"Создает новое голосование или показывает текущее (если имеется)", 
+				u"голосование [текст]", 
+				(u"голосование винды - сакс!", u"голосование"), 
 				CHAT)
-registerCommand(vote, u"мнение", 11, 
-				u"Для подачи мнения в голосовании", 
-				u"мнение <номер_голосования> <номер_пункта>", 
-				(u"мнение 2 4", ), 
+registerCommand(startVote, u"голосование+", 11, 
+				u"Возобновляет голосование", 
+				None, 
+				(u"голосование+", ), 
+				CHAT | NONPARAM)
+registerCommand(stopVote, u"голосование-", 11, 
+				u"Останавливает голосование, все данные сохраняются до продолжения голосования", 
+				u"голосование-", 
+				(u"голосование-"), 
+				CHAT | NONPARAM)
+registerCommand(addOpinion, u"пункт+", 11, 
+				u"Добавляет пункт к текущему голосованию", 
+				u"пункт+ <пункт>", 
+				(u"пункт+ да", ), 
+				CHAT | PARAM)
+registerCommand(delOpinion, u"пункт-", 11, 
+				u"Удаляет пункт из голосования. Пункт указывается его номером", 
+				u"пункт- <номер_пункта>", 
+				(u"пункт- 5", ), 
 				CHAT | PARAM)
 registerCommand(showOpinions, u"мнения", 11, 
 				u"Отдаёт текущие результаты голосования в приват, не завершая голосования при этом", 
-				u"мнения [номер]", 
-				(u"мнения", u"мнения 5"), 
-				CHAT)
+				None, 
+				(u"мнения", ), 
+				CHAT | NONPARAM)
 registerCommand(endVote, u"итоги", 11, 
-				u"Завершает голосование и выдает результаты", 
-				u"итоги [номер]",
-				(u"итоги", u"итоги 5"), 
-				CHAT)
+				u"Завершает голование и показывает его результаты", 
+				None, 
+				(u"итоги", ), 
+				CHAT | NONPARAM)
