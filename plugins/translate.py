6de87b8014f2c732b313f352b1a3d682d7d9d1b9
alexesprit <alex.esprit@gmail.com>
2010-12-11 21:36:09 +0000

    * translate: для автоопределения исходного языка текста достаточно не указывать его
    git-svn-id: http://snapi-bot.googlecode.com/svn/trunk@176 f251a5fa-d1d5-fdf8-25d6-16410800f524

--- plugins/translate.py
+++ plugins/translate.py
@@ -21,34 +21,18 @@ def loadLangsForTranslate():
 	path = getFilePath(RESOURCE_DIR, TRANSL_LANGS_FILE)
 	TRANSL_LANGS = eval(utils.readFile(path, "utf-8"))
 
-def getTranslateQuery(text, langpair=None):
-	param = {
-		"v": "1.0", 
-		"q": text.encode("utf-8")
-	}
-	if langpair:
-		param["langpair"] = langpair
-	return param
-
 def getTranslatedText(text, source, target):
 	url = "http://ajax.googleapis.com/ajax/services/language/translate"
-	langpair = "%s|%s" % (source, target)
-	qparam = getTranslateQuery(text, langpair)
+	qparam = {
+		"v": "1.0", 
+		"q": text.encode("utf-8"), 
+		"langpair": "%s|%s" % (source, target)
+	}
 	response = getURL(url, qparam)
 	if response:
 		answer = simplejson.load(response)
 		if answer["responseData"]:
 			return answer["responseData"]["translatedText"]
-	return None
-
-def detectLanguage(text):
-	url = "http://ajax.googleapis.com/ajax/services/language/detect"
-	qparam = getTranslateQuery(text)
-	response = getURL(url, qparam)
-	if response:
-		answer = simplejson.load(response)
-		if answer["responseData"]:
-			return answer["responseData"]["language"]
 	return None
 
 def translateText(msgType, conference, nick, param):
@@ -59,22 +43,22 @@ def translateText(msgType, conference, nick, param):
 		message = u"Доступные языки:\n%s" % ("\n".join(elements))
 		sendMsg(msgType, conference, nick, message)
 	else:
-		param = param.split(None, 2)
-		if len(param) == 3:
-			source, target, text = param
-			if source in TRANSL_LANGS and target in TRANSL_LANGS:
-				if source == "auto":
-					if target == "auto":
-						sendMsg(msgType, conference, nick, u"Читай помощь по команде")
-						return
+		args = param.split(None, 2)
+		if len(args) >= 2:
+			source = args[0]
+			target = args[1]
+			if source in TRANSL_LANGS:
+				# maybe target is a text
+				if target not in TRANSL_LANGS:
+					args = param.split(None, 1)
+					target, text = args
+					source = ""
+				else:
+					if 3 == len(args):
+						text = args[2]
 					else:
-						source = detectLanguage(text)
-						if source:
-							if not source in TRANSL_LANGS:
-								sendMsg(msgType, conference, nick, u"Не могу понять, что это за язык (%s)" % (source))
-								return
-						else:
-							sendMsg(msgType, conference, nick, u"Не могу перевести")
+						sendMsg(msgType, conference, nick, u"Вы не указали текст для перевода!")
+						return
 				text = getTranslatedText(text, source, target)
 				if text:
 					sendMsg(msgType, conference, nick, utils.unescapeHTML(text))
@@ -85,6 +69,6 @@ registerEventHandler(loadLangsForTranslate, EVT_STARTUP)
 
 registerCommand(translateText, u"перевод", 10, 
 				u"Перевод текста с одного языка на другой. Чтобы получить список доступных языков для перевода, укажите \"языки\" в кач-ве параметра", 
-				u"<исходный_язык> <нужный_язык> <фраза>", 
-				(u"языки", u"en ru hello", u"ru en привет"), 
+				u"[исходный_язык] <нужный_язык> <фраза>", 
+				(u"языки", u"en привет", u"en ru hello"), 
 				CMD_ANY | CMD_PARAM)
