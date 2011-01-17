14daf7a76c2f7eeb5cb67795729dac10f46ef7c8
alexesprit <alex.esprit@gmail.com>
2011-01-17 05:25:43 +0000

    + new plugin that updates working copy of bot
    git-svn-id: http://snapi-bot.googlecode.com/svn/trunk@224 f251a5fa-d1d5-fdf8-25d6-16410800f524

---
+++ plugins/update.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+# update.py
+# Initial Copyright (с) 2010 -Esprit-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+def updateWorkingCopy(msgType, conference, nick, param):
+	response = os.popen("svn up").read()
+	if response:
+		currentRev = gVerInfo.getRevision()
+		newRev = os.popen("svnversion -n").read()
+		if currentRev != newRev:
+			sendMsg(msgType, conference, nick, u"Обновлено до редакции %s" % (newRev))
+			prs = protocol.Presence(typ=protocol.PRS_OFFLINE)
+			prs.setStatus(u"Выключаюсь... (Обновление)")
+			gClient.send(prs)
+			shutdown(True)
+		else:
+			sendMsg(msgType, conference, nick, u"Ваша копия не требует обновления")
+	else:
+		sendMsg(msgType, conference, nick, u"Не удалось обновиться, возможно не установлена SubVersion")
+	
+registerCommand(updateWorkingCopy, u"update", 100, 
+				u"Обновление рабочей копии бота", 
+				None, 
+				None, 
+				CMD_ANY | CMD_NONPARAM)
