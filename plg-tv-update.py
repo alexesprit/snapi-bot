e64cdac7d64ea42ff225f1f930abad79ea8b3011
alexesprit <alex.esprit@gmail.com>
2013-04-26 17:11:16 +0000

    * Fixed tv updater * Completely fixed tv channels list
    git-svn-id: http://snapi-bot.googlecode.com/svn/trunk@325 f251a5fa-d1d5-fdf8-25d6-16410800f524

--- plg-tv-update.py
+++ plg-tv-update.py
@@ -11,10 +11,6 @@ TV_PROGRAM_FILE = 'tvchannels.txt'
 def getTVData():
 	data = netutil.getURLResponseData(S_TV_URL, encoding='utf-8')
 	if data:
-		print len(data)
-		# <a href="/old/tv/tv3/"><img src="http://img.s-tv.ru/logos/tv3.jpg" alt="ТВ-3" width="45px" title="ТВ-3" /></a>
-		# <a href="/old/tv/ntv/"><img src="http://img.s-tv.ru/logos/ntv.jpg" alt="" /><span>НТВ</span></a>
-		#elements = re.findall(r'<a href="(.+?)"><img.+?title="(.+?)" /></a>', data)
 		elements = re.findall(r'<a href="(.+?)"><img src=.+?><span>(.+?)</span></a>', data)
 		return elements
 	return None
@@ -23,7 +19,9 @@ out = open(TV_PROGRAM_FILE, 'wb')
 out.write('[\n')
 for data in getTVData():
 	chURL = data[0].encode('utf-8')
-	chName = data[1].encode('utf-8')
-	out.write('["%s", "%s"],\n' % (chName, chURL))
+	if chURL[-1] == '/':
+		chURL = chURL[:-1]
+	chName = data[1].encode('utf-8').strip()
+	out.write('[u"%s", "%s"],\n' % (chName, chURL))
 out.write(']\n')
 out.close()
